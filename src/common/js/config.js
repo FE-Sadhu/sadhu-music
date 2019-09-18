712e154a7973076d8d177cda48913e2bd264fa86
sadhu <1714222385@qq.com>
2019-09-19 01:38:58 +0800

    播放器player 的 vuex 设置 finish

---
+++ src/common/js/config.js
@@ -0,0 +1,7 @@
+// 配置文件 配置与项目相关的信息
+
+export const playMode = { // 播放模式
+  sequence: 0, // 顺序播放
+  loop: 1, // 循环播放
+  random: 2 // 随机播放
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import { playMode } from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false, // 播放就是 true
+  fullScreen: false, // 全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 排序播放列表
+  mode: playMode.sequence, // 播放的模式： 顺序或其他的,默认为顺序播放
+  currentIndex: -1 // 当前播放的索引 =》 (控制前进后退播放那些的)
 }
 
 export default state
