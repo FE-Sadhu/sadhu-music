2cdcfe23f29901d5d83e753c7279f7055d67f70e
sadhu <1714222385@qq.com>
2020-02-03 13:40:41 +0800

    修复 banner 接口

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -18,6 +18,7 @@
 
 import axios from 'axios'
 import { commonParams } from './config'
+import { _paramsSerializer } from 'common/js/utils'
 
 const debug = process.env.NODE_ENV !== 'production'
 
@@ -26,15 +27,42 @@ export function getSlider () {
   // const url = '/api/getSlider'
 
   const data = Object.assign({}, commonParams, {
-    uin: 0,
-    platform: 'h5',
-    needNewCode: 1
+    platform: 'yqq.json',
+    hostUin: 0,
+    needNewCode: 0,
+    inCharset: 'utf8',
+    format: 'json',
+    '-': 'recom' + (Math.random() + '').replace('0.', ''),
+    data: {
+      'comm': { 'ct': 24 },
+      'category': { 'method': 'get_hot_category', 'param': { 'qq': '' }, 'module': 'music.web_category_svr' },
+      'recomPlaylist': {
+        'method': 'get_hot_recommend',
+        'param': { 'async': 1, 'cmd': 2 },
+        'module': 'playlist.HotRecommendServer'
+      },
+      'playlist': {
+        'method': 'get_playlist_by_category',
+        'param': { 'id': 8, 'curPage': 1, 'size': 40, 'order': 5, 'titleid': 8 },
+        'module': 'playlist.PlayListPlazaServer'
+      },
+      'new_song': { 'module': 'newsong.NewSongServer', 'method': 'get_new_song_info', 'param': { 'type': 5 } },
+      'new_album': {
+        'module': 'newalbum.NewAlbumServer',
+        'method': 'get_new_album_info',
+        'param': { 'area': 1, 'sin': 0, 'num': 10 }
+      },
+      'new_album_tag': { 'module': 'newalbum.NewAlbumServer', 'method': 'get_new_album_area', 'param': {} },
+      'toplist': { 'module': 'musicToplist.ToplistInfoServer', 'method': 'GetAll', 'param': {} },
+      'focus': { 'module': 'QQMusic.MusichallServer', 'method': 'GetFocus', 'param': {} }
+    }
   })
 
   return axios.get(url, {
-    params: data
+    params: data,
+    paramsSerializer: _paramsSerializer
   }).then(res => {
-    return Promise.resolve(res.data)
+    return res.data
   })
 }
 
--- vue.config.js
+++ vue.config.js
@@ -10,15 +10,39 @@ module.exports = {
   devServer: {
     before (app) {
       app.get('/api/getSlider', function (req, res) {
-        const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+        const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+        const jumpPrefix = 'https://y.qq.com/n/yqq/album/'
+
         axios.get(url, {
           headers: {
-            referer: 'https://c.y.qq.com',
-            host: 'c.y.qq.com'
+            referer: 'https://u.y.qq.com/',
+            host: 'u.y.qq.com'
           },
           params: req.query
         }).then((response) => {
-          res.json(response.data)
+          response = response.data
+          if (response.code === 0) {
+            const slider = []
+            const content = response.focus.data && response.focus.data.content
+            if (content) {
+              for (let i = 0; i < content.length; i++) {
+                const item = content[i]
+                const sliderItem = {}
+                sliderItem.id = item.id
+                sliderItem.linkUrl = jumpPrefix + item.jump_info.url + '.html'
+                sliderItem.picUrl = item.pic_info.url
+                slider.push(sliderItem)
+              }
+            }
+            res.json({
+              code: 0,
+              data: {
+                slider
+              }
+            })
+          } else {
+            res.json(response)
+          }
         }).catch((e) => {
           console.log(e)
         })
